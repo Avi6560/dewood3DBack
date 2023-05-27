0848ce24e737121d3fe6b47e275fd742825e93d2
Avinash Kumar <avinashkumar151199@gmail.com>
2023-05-27 13:56:01 +0530

    new changes

--- models/imageModel.js
+++ models/imageModel.js
@@ -1,12 +1,14 @@
-const mongoose= require('mongoose');
+const mongoose = require("mongoose");
 
-const imageSchema= new mongoose.Schema({
-    name:{type:String, required:true},
-    price:{type:String, required:true},
-    collectionType:[{type:String, required:true}],
-    description:{type:String,required:true},
-    images:{type:Array},
-},{timestamps:true})
+const imageSchema = new mongoose.Schema(
+  {
+    name: { type: String },
+    price: { type: String },
+    collectionType: [{ type: String }],
+    description: { type: String },
+    images: { type: Array },
+  },
+  { timestamps: true }
+);
 
-module.exports =mongoose.model('images',imageSchema)
-
+module.exports = mongoose.model("images", imageSchema);
