2c57b6f5ddf1bfaa9e02d3bfdd77e57bf453a385
abdulbiztech <abdulbiztech@gmail.com>
2023-05-30 18:53:35 +0530

    backend working fine

--- models/imageModel.js
+++ models/imageModel.js
@@ -4,7 +4,7 @@ const imageSchema = new mongoose.Schema(
   {
     name:{type:String},
     description:{type:String},
-    price:{type:String},
+    price:{type:Number},
     imageUrls: { type: Array },
     collectionType: { type: Array },
   },
