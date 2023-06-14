b3f8881a5c774d0d049455fb8126e37b874a0383
abdulbiztech <abdulbiztech@gmail.com>
2023-06-14 18:54:03 +0530

    wednesday commit

--- controllers/buyController.js
+++ controllers/buyController.js
@@ -1,63 +1,75 @@
-const Item= require('../models/itemModel')
-const User= require('../models/userModel')
-const Buy= require('../models/buyModel')
-const Razorpay= require('razorpay')
-const crypto= require('crypto')
+const Item = require("../models/itemModel");
+const User = require("../models/userModel");
+const Buy = require("../models/buyModel");
+const Razorpay = require("razorpay");
+const crypto = require("crypto");
 
-let razorpay = new Razorpay({key_id:'rzp_test_rctEhk9DkJO7hU',key_secret:'dH3vdnM9vuumH6JkYg8mSoJB'})
+let razorpay = new Razorpay({
+  key_id: "rzp_test_rctEhk9DkJO7hU",
+  key_secret: "dH3vdnM9vuumH6JkYg8mSoJB",
+});
 
+const createOrder = async (req, res) => {
+  try {
+    const { amount, currency, receipt, notes } = req.body;
+    razorpay.orders.create(
+      { amount: amount * 100, currency, receipt, notes },
+      (err, order) => {
+        if (!err) res.json(order);
+        else res.send(err);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const createOrder= async(req,res)=>{
-    try {
-        const {amount,currency,receipt, notes}= req.body;
-        razorpay.orders.create({amount:amount*100, currency, receipt, notes},
-            (err, order)=>{
-              if(!err)
-                res.json(order)
-              else
-                res.send(err);
-            }
-        )
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-
-const  checkout = async (req, res) => {
-    // console.log("he");
-    // console.log(req.body,"mere body");
-    var options = {
-      amount: Number(req.body.amount * 100), // amount in the smallest currency unit
-      currency: "INR",
-    };
-    const order = await razorpay.orders.create(options);
-    // console.log(order,"mai order hu");
-    res.status(200).json({
-      success: true,
-      order,
-    });
+const checkout = async (req, res) => {
+  // console.log("he");
+  // console.log(req.body,"mere body");
+  var options = {
+    amount: Number(req.body.amount * 100), // amount in the smallest currency unit
+    currency: "INR",
   };
+  const order = await razorpay.orders.create(options);
+  // console.log(order,"mai order hu");
+  res.status(200).json({
+    success: true,
+    order,
+  });
+};
 
+const verifyOrder = async (req, res) => {
+  console.log(req.body);
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+    req.body;
 
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
 
-const verifyOrder = async(req,res)=>{
-    console.log("body",req.body);
-    // try {
-    // const {order_id, payment_id} = req.body;
-    // const razorpay_signature =  req.headers['x-razorpay-signature'];
+  const expectedSignature = crypto
+    .createHmac("sha256", key_secret)
+    .update(body.toString())
+    .digest("hex");
+    console.log("recived",  razorpay_signature);
+    console.log("genrate",  expectedSignature );
+    const isAuthentic = expectedSignature === razorpay_signature;
+    if (isAuthentic) {
+      // Database comes here
 
-    // const key_secret = dH3vdnM9vuumH6JkYg8mSoJB;
-    // let hmac = crypto.createHmac('sha256', key_secret);
-    // hmac.update(order_id + "|" + payment_id);
-    // const generated_signature = hmac.digest('hex');
+      await Payment.create({
+        razorpay_order_id,
+        razorpay_payment_id,
+        razorpay_signature,
+      });
 
-    // if(razorpay_signature===generated_signature){
-    //     res.json({success:true, message:"Payment has been verified"})
-    // }else
-    // res.json({success:false, message:"Payment verification failed"})
-    // } catch (error) {
-    //     console.log(error);
-    // }
-}
-module.exports ={createOrder, checkout,verifyOrder}
+      res.redirect(
+        `http://127.0.0.1:5173/paymentsuccess?reference=${razorpay_payment_id}`
+      );
+    } else {
+      res.status(400).json({
+        success: false,
+      });
+    }
+
+};
+module.exports = { createOrder, checkout, verifyOrder };
