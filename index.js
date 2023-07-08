44018e541ac151ae44a24b799a55ef4cdf4a7010
abdulbiztech <abdulbiztech@gmail.com>
2023-07-08 16:57:16 +0530

    images delete 5

--- index.js
+++ index.js
@@ -30,28 +30,11 @@ mongoose
 
 app.get("/image/:filename", (req, res) => {
   const fileName = req.params.filename;
-  // console.log(__dirname);
   const filePath = __dirname + "/uploads/" + fileName;
 
   res.sendFile(filePath);
 });
 app.use("/", route);
-
-
-// let razorpay = new Razorpay({key_id:'rzp_test_rctEhk9DkJO7hU',key_secret:'dH3vdnM9vuumH6JkYg8mSoJB'})
-
-// app.post('/payment',async(req,res)=>{
-//   const {amount}=req.body
-
-//   razorpay.orders.create({
-//     amount:amount*100,
-//     currency: 'USD',
-//     receipt:"receipt#1",
-
-//   })
-//   return res.status(201).json({status:true, amount })
-// })
-
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
