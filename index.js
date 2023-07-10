d0cb33fd0b48d3b74a59d37a6925c6c37ba4336a
abdulbiztech <abdulbiztech@gmail.com>
2023-07-10 18:24:59 +0530

    aws deploy

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors(["https://deod-new.vercel.app/", "http://localhost:4200"]));
+app.use(cors(["http://13.232.195.137:5000/","http://localhost:5000/"]));
 app.use(express.json());
 mongoose.set("strictQuery", true);
 mongoose
